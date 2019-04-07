c03ef067123ee02005de8826a8468dbe54d92e5e
Gveir <kri5s.prog@gmail.com>
2019-04-07 23:34:43 +0200

    Added task completed and due date

--- react/client/src/actions/index.js
+++ react/client/src/actions/index.js
@@ -5,7 +5,13 @@ export const fetchTasks = () => {
     type: FETCH_TASKS,
     payload: [
       { id: 1, title: 'Task 1', completed: false },
-      { id: 2, title: 'Task 2', completed: true }
+      { id: 2, title: 'Task 2', completed: true },
+      {
+        id: 3,
+        title: 'Task 3',
+        completed: true,
+        dueDate: new Date(2019, 4, 15)
+      }
     ]
   };
 };
--- react/client/src/components/TaskCard.js
+++ react/client/src/components/TaskCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 
 const styles = () => ({
@@ -14,9 +15,20 @@ const TaskCard = ({ task, classes }) => {
   return (
     <Card className={classes.card}>
       <CardContent>
+        <Checkbox
+          id="task-completed"
+          checked={task.completed}
+          color="primary"
+          className={classes.checkbox}
+        />
         <Typography variant="h6" color="textPrimary" gutterBottom>
           {task.title}
         </Typography>
+        {task.dueDate && (
+          <Typography variant="body2">
+            Due date: {task.dueDate.toLocaleDateString()}
+          </Typography>
+        )}
       </CardContent>
     </Card>
   );
