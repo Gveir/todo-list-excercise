66173d48a8f67729727b5d10bf2ba6a31338dd25
Gveir <kri5s.prog@gmail.com>
2019-04-22 15:37:36 +0200

    Added grid layout for task card

--- react/client/src/components/TaskCard.js
+++ react/client/src/components/TaskCard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -29,28 +30,51 @@ class TaskCard extends React.Component {
     return (
       <Card className={classes.card}>
         <CardContent>
-          <IconButton
-            id="task-completed"
-            color="primary"
-            onClick={() => this.onTaskCompleteClicked()}
+          <Grid
+            container
+            spacing={8}
+            alignItems="flex-start"
+            direction="column"
           >
-            <Icon fontSize="large">
-              {task.completed ? 'check_box' : 'check_box_outline_blank'}
-            </Icon>
-          </IconButton>
-          <Typography
-            className={task.completed ? classes.titleCompleted : ''}
-            variant="h6"
-            color="textPrimary"
-            gutterBottom
-          >
-            {task.title}
-          </Typography>
-          {task.dueDate && (
-            <Typography variant="body2">
-              Due date: {task.dueDate.toLocaleDateString()}
-            </Typography>
-          )}
+            <Grid
+              item
+              container
+              justify="flex-start"
+              alignItems="center"
+              direction="row"
+            >
+              <Grid item>
+                <IconButton
+                  id="task-completed"
+                  color="primary"
+                  onClick={() => this.onTaskCompleteClicked()}
+                >
+                  <Icon fontSize="large">
+                    {task.completed ? 'check_box' : 'check_box_outline_blank'}
+                  </Icon>
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <Typography
+                  className={task.completed ? classes.titleCompleted : ''}
+                  variant="h6"
+                  color="textPrimary"
+                >
+                  {task.title}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item container direction="row">
+              <Grid item />
+              <Grid item>
+                {task.dueDate && (
+                  <Typography variant="body2">
+                    Due date: {task.dueDate.toLocaleDateString()}
+                  </Typography>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     );
