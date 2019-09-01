164447fd5f2b1061ac115377a443b0a278dec7a8
Gveir <kri5s.prog@gmail.com>
2019-09-01 17:28:55 +0200

    Added "New task" button

--- react/client/src/components/App.js
+++ react/client/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
@@ -10,6 +12,9 @@ const styles = () => ({
   root: {
     maxWidth: 1024,
     margin: 'auto'
+  },
+  title: {
+    flexGrow: 1
   }
 });
 
@@ -19,9 +24,12 @@ const App = props => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="inherit" className={classes.title}>
             Tasks
           </Typography>
+          <IconButton color="inherit">
+            <Icon fontSize="large">add</Icon>
+          </IconButton>
         </Toolbar>
       </AppBar>
       <TaskList />
