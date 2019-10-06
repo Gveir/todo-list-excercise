77711805e904237df11d8db89a489800c6aba023
Gveir <kri5s.prog@gmail.com>
2019-10-06 10:21:38 +0200

    Scaffolded back-end service

---
+++ backend/TodoList/TodoList/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Task> GetTasks()
+        {
+            return new List<Task>();
+        }
+    }
+}
---
+++ backend/TodoList/TodoList/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+        public uint Id { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
